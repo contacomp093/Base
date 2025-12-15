d6fb7e46eb70c43f5020fd21e9dc6a59690589e2
contacomp093 <contacomp093@gmail.com>
2025-12-14 23:07:15 -0600

    migracion

--- Base_Login/manage.py
+++ Base_Login/manage.py
@@ -3,9 +3,14 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
 
 def main():
     """Run administrative tasks."""
+    # Carga variables desde .env (solo existe en local)
+    load_dotenv()
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
